1713249c52c8c613db99c37b2153f587fe00c7fb
Kenny Evitt <kevitt@recp.com>
2014-08-29 10:43:14 -0400

    Add initial version of files.

---
+++ FitNesse/release_20130530/run-fitnesse.bat
@@ -0,0 +1,3 @@
+REM http://fitnesse.org/FitNesse.UserGuide.CommandLineOption
+start "FitNesse" "C:\Program Files (x86)\Java\jre7\bin\java.exe" -jar fitnesse-standalone.jar -p 8888 -e 0
+start http://localhost:8888
